e4222a175540d36fafcc171a3f58c2e8fc4dd1f7
Yorgan-Agb <agblemagnon.yorgan@gmail.com>
2025-11-30 17:03:16 +0100

    route auth/refrresh ok + test ok

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -3,8 +3,13 @@ import { registerSchema, loginSchema } from "../validations/auth.validation.ts";
 import { login, register } from "../services/auth.service.ts";
 import { setRefreshTokenCookie } from "../lib/cookie.ts";
 import { prisma } from "../models/index.ts";
-import { generateAccessToken, generateRefreshToken } from "../lib/auth.ts";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  extractRefreshTokenFromReq,
+} from "../lib/auth.ts";
 import { NotFoundError } from "../lib/error.ts";
+import e from "express";
 
 export const registerUser = async (req: Request, res: Response) => {
   const userData = registerSchema.parse(req.body);
@@ -18,4 +23,33 @@ export const loginUser = async (req: Request, res: Response) => {
   const { accessToken, refreshToken } = await login(email, password, userAgent);
   setRefreshTokenCookie(res, refreshToken);
   res.status(200).json({ accessToken, refreshToken });
+};
+
+// Extraire le refresh token depuis la requête
+// Décoder et vérifier le refresh token (JWT valide ?)
+// Vérifier que le token existe en BDD et n'est pas expiré
+// Générer un nouveau access token
+// (Optionnel) Générer un nouveau refresh token
+// Mettre à jour lastUsedAt du refresh token en BDD
+// Retourner le(s) nouveau(x) token(s)
+
+export const tokenRefresh = async (req: Request, res: Response) => {
+  const extractToken = req.cookies["refreshToken"];
+  extractRefreshTokenFromReq(extractToken);
+  const storedToken = await prisma.refreshToken.findUnique({
+    where: { token: extractToken },
+  });
+  if (!storedToken) {
+    throw new NotFoundError("Refresh token not found");
+  }
+  if (storedToken.expire_at < new Date()) {
+    throw new NotFoundError("Refresh token expired");
+  }
+  const user = await prisma.user.findUnique({
+    where: { id: storedToken.userId },
+  });
+  if (!user) {
+    throw new NotFoundError("User not found");
+  }
+  const newAccessToken = generateAccessToken;
 };
--- api/src/routers/auth.router.ts
+++ api/src/routers/auth.router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/auth.controller.ts";
+import { tokenRefresh } from "../controllers/auth.controller.ts";
+
 export const authRouter = Router();
 
 authRouter.post("/register", registerUser);
 authRouter.post("/login", loginUser);
+authRouter.post("/refresh", tokenRefresh);
