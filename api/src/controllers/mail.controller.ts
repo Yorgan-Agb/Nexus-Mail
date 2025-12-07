6aade2064977e2993fdfebce5b3bea6c6ae3eb65
Yorgan-Agb <agblemagnon.yorgan@gmail.com>
2025-12-07 23:59:33 +0100

    branche mails en attente, passage vers la branche folders car folderId obligatoire pour la création d'un mail

--- api/src/controllers/mail.controller.ts
+++ api/src/controllers/mail.controller.ts
@@ -1,6 +1,10 @@
 import { decodeAccessToken, extractAccessTokenFromReq } from "../lib/auth.ts";
 import type { Request, Response } from "express";
-import { allMails, getMailById } from "../services/mail.service.ts";
+import {
+  allMails,
+  getMailById,
+  sendMailUser,
+} from "../services/mail.service.ts";
 import { UnauthorizedError } from "../lib/error.ts";
 
 export const getAllMails = async (req: Request, res: Response) => {
@@ -28,3 +32,5 @@ export const getOneMail = async (req: Request, res: Response) => {
 
   res.status(200).json({ mail });
 };
+
+export const sendMail = async (req: Request, res: Response) => {};
--- api/src/routers/mail.router.ts
+++ api/src/routers/mail.router.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth.middleware.ts";
-import { getAllMails, getOneMail } from "../controllers/mail.controller.ts";
+import {
+  getAllMails,
+  getOneMail,
+  sendMail,
+} from "../controllers/mail.controller.ts";
 
 export const mailRouter = Router();
 
 mailRouter.get("/", checkAuth, getAllMails);
 mailRouter.get("/:id", checkAuth, getOneMail);
+mailRouter.post("/send", checkAuth, sendMail);
